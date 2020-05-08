499467c6855b074393a4d23e560d8460a4ef5cf2
hollson <hollson@qq.com>
2020-05-08 13:21:33 +0800

    Rebuilded at 2020年05月08日 13时21分33秒

--- proto/gen.sh
+++ proto/gen.sh
@@ -3,17 +3,34 @@
 rm -rf ./_gen
 mkdir -p ./_gen/go ./_gen/py ./_gen/php ./_gen/cs ./_gen/java
 
-: '
-功能：生成相应编程语言的protobuf文件
+#proto2语法规范参考：
+#https://developers.google.com/protocol-buffers/docs/reference/proto2-spec
+#proto3语法规范参考：
+#https://developers.google.com/protocol-buffers/docs/reference/proto3-spec
+
+##注意事项(libprotoc 3.9.0)：
+# 1. C#只支持proto3语法
+# 2. PHP只支持proto3语法
+
+
+:'
+功能：执行编译命令
 用法：protoc [OPTION] PROTO_FILES
 参数：-I 指定一组ptoto文件
 '
 protoc -I ./ \
 --go_out=plugins=grpc:./_gen/go \
---csharp_out=./_gen/cs/ \
 --java_out=./_gen/java/ \
---php_out=./_gen/php/ \
 --python_out=./_gen/py/ \
 ./*.proto
+
+# 多语言版本
+#protoc -I ./ \
+#--go_out=plugins=grpc:./_gen/go \
+#--csharp_out=./_gen/cs/ \
+#--java_out=./_gen/java/ \
+#--php_out=./_gen/php/ \
+#--python_out=./_gen/py/ \
+#./*.proto
 
 echo " ✅ proto文件编译完成！！！"
