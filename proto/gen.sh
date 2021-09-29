6ae1b7e768234579f54aa3aa990af847b03e293e
hollson <hollson@qq.com>
2021-09-29 14:03:48 +0800

    Rebuilded at 2021年09月29日 14时03分48秒

--- proto/gen.sh
+++ proto/gen.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 rm -rf ./_gen
-mkdir -p ./_gen/go ./_gen/py ./_gen/java
+mkdir -p ./_gen/go ./_gen/py ./_gen/java ./_gen/cs
 
 #proto2语法规范参考：
 #https://developers.google.com/protocol-buffers/docs/reference/proto2-spec
@@ -18,18 +18,16 @@ mkdir -p ./_gen/go ./_gen/py ./_gen/java
 # 1. C#只支持proto3语法
 # 2. PHP只支持proto3语法
 
-protoc -I ./ \
---go_out=plugins=grpc:./_gen/go \
---java_out=./_gen/java/ \
---python_out=./_gen/py/ \
-./*.proto
+#protoc -I ./ \
+#--go_out=./_gen/go \
+#--java_out=./_gen/java/ \
+#--python_out=./_gen/py/ \
+#./*.proto
 
 
 # 多语言版本
-#protoc -I ./ \
-#--go_out=plugins=grpc:./_gen/go \
-#--csharp_out=./_gen/cs/ \
-#--java_out=./_gen/java/ \
-#--php_out=./_gen/php/ \
-#--python_out=./_gen/py/ \
-#./*.proto
+protoc -I ./ \
+--go_out=plugins=grpc:../gen/go \
+--csharp_out=./_gen/cs/ \
+--java_out=./_gen/java/ \
+./*.proto
--- version.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2020 Hollson. All rights reserved.
-// Use of this source code is governed by a MIT style
-// license that can be found in the LICENSE file.
-
-package main
-
-// Version is the current gin framework's version.
-const Version = "v1.6.3"
